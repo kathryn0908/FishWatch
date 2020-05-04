a133eda67ff6a3264febb616300b1194d0512aad
kathryn0908 <kathrynlael@gmail.com>
2020-05-04 15:45:03 -0600

    new API method

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,83 +9,103 @@ require 'pry'
 require 'rest-client'
 require 'json'
 
-response = RestClient.get("https://www.fishwatch.gov/api/species")
-    fishdata = JSON.parse(response)
 
-def get_fish_species_name
+def get_fish
     response = RestClient.get("https://www.fishwatch.gov/api/species")
     fishdata = JSON.parse(response)
     
-    fishes = fishdata
     
-    fishes.each_with_index do |fish, index|
-        if index < 25
-        species_name = fish["Species Name"]
-        species_name
-        # byebug
-        end
+   new_fish_data = fishdata.each_with_index do |fish, index|
+            if index < 2
+            {species_name: fish["Species Name"],
+            scientific_name: fish["Scientific Name"],
+            location: fish["Location"],
+            habitat: fish["Habitat"],
+            species_illustration_photo: fish["Species Illustration Photo"]
+        }
+        end   
+        new_fish_data
+        
     end
-end
+    byebug
 
-def get_fish_scientific_name
-    response = RestClient.get("https://www.fishwatch.gov/api/species")
-    fishdata = JSON.parse(response)
-    
-    fishes = fishdata
-    
-    fishes.each_with_index do |fish, index|
-        if index < 25
-        scientific_name = fish["Scientific Name"]
-        scientific_name
+    new_fish_data.map do |fish|
+        if fish
+        Fish.create(
+            species_name: fish[:species_name],
+            scientific_name: fish[:scientific_name],
+            habitat: fish[:habitat],
+            location: fish[:location],
+            species_illustration_photo: params[:species_illustration_photo]
+        )
         end
+           
     end
-end
-
-def get_fish_location
-    response = RestClient.get("https://www.fishwatch.gov/api/species")
-    fishdata = JSON.parse(response)
-    
-    fishes = fishdata
-    
-    fishes.each_with_index do |fish, index|
-        if index < 25
-        location = fish["Location"]
-        location
-        end
+        
     end
-end 
+   
+    get_fish
+ 
 
-def get_fish_habitat
-    response = RestClient.get("https://www.fishwatch.gov/api/species")
-    fishdata = JSON.parse(response)
+# def get_fish_scientific_name
+#     response = RestClient.get("https://www.fishwatch.gov/api/species")
+#     fishdata = JSON.parse(response)
     
-    fishes = fishdata
+#     fishes = fishdata
     
-    fishes.each_with_index do |fish, index|
-        if index < 25
-        habitat = fish["Habitat"]
-        habitat
-        end
-    end
-end
+#     fishes.each_with_index do |fish, index|
+#         if index < 25
+#         scientific_name = fish["Scientific Name"]
+#         scientific_name
+#         end
+#     end
+# end
 
-def get_fish_illustration
-    response = RestClient.get("https://www.fishwatch.gov/api/species")
-    fishdata = JSON.parse(response)
+# def get_fish_location
+#     response = RestClient.get("https://www.fishwatch.gov/api/species")
+#     fishdata = JSON.parse(response)
     
-    fishes = fishdata
+#     fishes = fishdata
     
-    fishes.each_with_index do |fish, index|
-        if index < 25
-        illustration = fish["Species Illustration Photo"]
-        illustration
-        end
-    end
-end
+#     fishes.each_with_index do |fish, index|
+#         if index < 25
+#         location = fish["Location"]
+#         location
+#         end
+#     end
+# end 
+
+# def get_fish_habitat
+#     response = RestClient.get("https://www.fishwatch.gov/api/species")
+#     fishdata = JSON.parse(response)
+    
+#     fishes = fishdata
+    
+#     fishes.each_with_index do |fish, index|
+#         if index < 25
+#         habitat = fish["Habitat"]
+#         habitat
+#         end
+#     end
+# end
+
+# def get_fish_illustration
+#     response = RestClient.get("https://www.fishwatch.gov/api/species")
+#     fishdata = JSON.parse(response)
+    
+#     fishes = fishdata
+    
+#     fishes.each_with_index do |fish, index|
+#         if index < 25
+#         illustration = fish["Species Illustration Photo"]
+#         illustration
+#         end
+#     end
+# end
 
 
-get_fish_habitat
-get_fish_species_name
-get_fish_scientific_name
-get_fish_location
-get_fish_illustration
+
+
+
+# Octopus = Fish.create( species_name:"Octopus", scientific_name:"Scientific Octopus", habitat:"the Ocean", location:"Around",
+#      species_illustration_photo:"Image here")
