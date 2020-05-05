ebfda2c326153dd8fa5cfb727e45be09ba2c30d6
kathryn0908 <kathrynlael@gmail.com>
2020-05-05 11:33:35 -0600

    updated the photo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
 require 'pry'
 require 'rest-client'
 require 'json'
+# require_relative '/MOD2_PROJECT/fish_api/app/controllers/fish_controller.rb'
 
 
 def get_fish
@@ -20,9 +15,13 @@ def get_fish
                 scientific_name: fish["Scientific Name"],
                 location: fish["Location"],
                 habitat: fish["Habitat"],
-                species_illustration_photo: fish["Species Illustration Photo"]
+                species_illustration_photo: fish["Species Illustration Photo"]["src"]
+                
             }
-        end   
+            
+        end  
+       
+
         
     
     new_fish_data.each_with_index do |fish, index|
@@ -34,11 +33,10 @@ def get_fish
             location: fish[:location],
             species_illustration_photo: fish[:species_illustration_photo]
         )
-        
         end
     end
 end
-        
+
     
    
     get_fish
